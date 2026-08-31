17de43503517967c2c13d818ebb4ac4b4e663bb1
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-08-31 17:00:50 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use rand::Rng;
+use macroquad::prelude::*;
 
+fn window_conf() -> Conf {
+    Conf {
+        window_width: 800,
+        window_height: 600,
+        ..Default::default()
+    }
+}
 #[macroquad::main("Pong")]
 async fn main() {
-    
+    let mut x = 10.0;
+    let mut y = 240.0;
+    let paddle_x = 20.0;
+    let paddle_y = 60.0;
+    loop {
+        clear_background(BLACK);
+        if is_key_down(KeyCode::W) {
+            y -= 5;
+        }
+        if is_key_down(KeyCode::S) {
+            y += 5;
+        }
+        draw_rectangle(x,y,paddle_x,paddle_y);
+    }
 }
